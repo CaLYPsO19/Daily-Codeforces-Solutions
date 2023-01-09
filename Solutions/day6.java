a673e410f721b3b67b410b0f6a0d3ff1866a529b
Sayani Murmu <93235923+CaLYPsO19@users.noreply.github.com>
2023-01-09 10:03:02 +0530

    Create day6.java

---
+++ Solutions/day6.java
@@ -0,0 +1,38 @@
+/*
+71A - Way Too Long Words
+Sometimes some words like "localization" or "internationalization" are so long that writing them many times in one text is quite tiresome.
+
+Let's consider a word too long, if its length is strictly more than 10 characters. All too long words should be replaced with a special abbreviation.
+
+This abbreviation is made like this: we write down the first and the last letter of a word and between them we write the number of letters between the first and the last letters. That number is in decimal system and doesn't contain any leading zeroes.
+
+Thus, "localization" will be spelt as "l10n", and "internationalization» will be spelt as "i18n".
+
+You are suggested to automatize the process of changing the words with abbreviations. At that all too long words should be replaced by the abbreviation and the words that are not too long should not undergo any changes.
+*/
+import java.util.Scanner;
+public class Problem71A {
+	
+	static Scanner sr = new Scanner(System.in);
+	public static void main(String args[])
+	{
+		double length = sr.nextInt();
+		String in = null;
+		
+		for(int i = 0; i <= length; i++)
+		{
+			in = sr.nextLine();
+			
+			if(in.length() > 10)
+			{
+				System.out.print(in.charAt(0));
+				System.out.print(in.length() -2); 
+				System.out.println(in.charAt(in.length() -1));
+			}else
+				System.out.println(in);
+			
+		}
+	}
+
+
+}
