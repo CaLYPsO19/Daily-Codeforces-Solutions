d1612a1ef128406904193fb87f9f545d9a6f9d05
Sayani Murmu <93235923+CaLYPsO19@users.noreply.github.com>
2023-01-28 23:21:26 +0530

    Create #025.java

---
+++ Solutions/#025.java
@@ -0,0 +1,35 @@
+/*
+Presents
+Little Petya very much likes gifts. Recently he has received a new laptop as a New Year gift from his mother. He immediately decided to give it to somebody else as what can be more pleasant than giving somebody gifts. And on this occasion he organized a New Year party at his place and invited n his friends there.
+
+If there's one thing Petya likes more that receiving gifts, that's watching others giving gifts to somebody else. Thus, he safely hid the laptop until the next New Year and made up his mind to watch his friends exchanging gifts while he does not participate in the process. He numbered all his friends with integers from 1 to n. Petya remembered that a friend number i gave a gift to a friend number pi. He also remembered that each of his friends received exactly one gift.
+
+Now Petya wants to know for each friend i the number of a friend who has given him a gift.
+*/
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class presents
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if((a[j]-1)==i)
+				{
+				System.out.print(j+1+" ");
+				break;
+				}
+			}
+		}
+	}
+}
