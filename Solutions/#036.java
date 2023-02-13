b9fd728e67bc31ad1b424314caffa3c1e9bada63
Sayani Murmu <93235923+CaLYPsO19@users.noreply.github.com>
2023-02-13 23:21:56 +0530

    Create #036.java

---
+++ Solutions/#036.java
@@ -0,0 +1,15 @@
+/*
+The New Year: Meeting Friends
+*/
+import java.util.Scanner;
+public class newy {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        int min = Math.min(a, Math.min(b,c));
+        int max = Math.max(a,Math.max(b,c));
+        System.out.println(max-min);
+    }   
+}
